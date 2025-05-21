72a5673f72490c79f8ec4113c61424df5299be80
vaishnu1 <vaishnu753880@gmail.com>
2025-05-21 13:31:52 +0530

    Initial commit of DSA-Scratch files

---
+++ Basicloop.java
@@ -0,0 +1,11 @@
+public class Basicloop{
+    public static void main(String[] args)
+    {
+        
+        for(int i=0; i<=10;i++)
+        {
+            System.out.println(i);
+        }
+    }
+
+} 
